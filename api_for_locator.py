0fbd8d176adf06c567d55dc93e11c9c0ffee238a
DiLysenk <46103863+DiLysenk@users.noreply.github.com>
2020-02-03 19:39:47 +0300

    добавлена логика на отсеивание пустых значений

--- api_for_locator.py
+++ api_for_locator.py
@@ -1,66 +1,73 @@
 import requests
 import time
 
-
-
 # url - адрес запроса
 url = 'http://api.lbs.yandex.net/geolocation'
 
-
 # ответ если базовой станции не существует
-response_BS0 = "{
-   "position":
-      {
-         "latitude":55.75321578979492,
-         "longitude":37.62250518798828,
-         "altitude":0.0,
-         "precision":100000.0,
-         "altitude_precision":30.0,
-         "type":"ip"
-      }
-}"
+
 
 # ключ от yandex локатора
-def key_yandex():
-    global key
-    key = "AAWW1l0BAAAA-ySjGQIAJGp8BARUCie1PUbaznYu58C8xzcAAAAAAAAAAABF0LkL4ONOCjhC_WbHUrN__bCa6w=="
+key = "AAWW1l0BAAAA-ySjGQIAJGp8BARUCie1PUbaznYu58C8xzcAAAAAAAAAAABF0LkL4ONOCjhC_WbHUrN__bCa6w=="
 
 # создаем список для ответок
 response_list = []
 
-
 # Открытие файла
-key_yandex()
 file = open('response.txt', 'w')
-for cid in range(4000, 4002):
-    time.sleep(0.01)
-    payload = "json={\r\n   \"common\": {\r\n      \"version\": \"1.0\",\r\n      \"api_key\": " \
+for cid in range(4100, 4150):                       # 4131, 4132, 4133
+    time.sleep(0.001)
+    payload = "json={\n   \"common\": {\n      \"version\": \"1.0\",\n      \"api_key\": " \
               "\"" + key + "\"\r\n   }," \
-              "\r\n   \"gsm_cells\": [\r\n       {\r\n          \"countrycode\": 250,\r\n          \"operatorid\": 99," \
-              "\r\n          \"cellid\": " + str(cid) + ",\r\n          \"lac\": 14752,\r\n   \"signal_strength\": -80," \
-              "\r\n   \"age\": 1000\r\n    }\r\n   ],\r\n   \"wifi_networks\": [\r\n       {\r\n          " \
-              "\"mac\": \"2CD02D814C80\",\r\n          \"signal_strength\": -68,\r\n          \"age\": 500,\r\n       }," \
-              "\r\n       {\r\n          \"mac\": \"E4AA5DE28CD0\",\r\n          \"signal_strength\": -60,\r\n          " \
-              "\"age\": 500,\r\n       }\r\n   ],\r\n   \"ip\": {\r\n     \"address_v4\": \"95.108.173.231\"\r\n   }\r\n} "
+                           "\r\n   \"gsm_cells\": [\r\n       {\r\n          \"countrycode\": 250,\r\n          " \
+                           "\"operatorid\": 99," \
+                           "\r\n          \"cellid\": " + str(
+        cid) + ",\r\n          \"lac\": 14752,\r\n   \"signal_strength\": -80," \
+               "\r\n   \"age\": 1000\r\n    }\r\n   ],\r\n   \"wifi_networks\": [\r\n       {\r\n          " \
+               "\"mac\": \"2CD02D814C80\",\r\n          \"signal_strength\": -68,\r\n          \"age\": 500," \
+               "\r\n       }," \
+               "\r\n       {\r\n          \"mac\": \"E4AA5DE28CD0\",\r\n          \"signal_strength\": -60,\r\n       " \
+               "   " \
+               "\"age\": 500,\r\n       }\r\n   ],\r\n   \"ip\": {\r\n     \"address_v4\": \"95.108.173.231\"\r\n   " \
+               "}\r\n} "
     headers = {'content-type': 'application/json'}
-    response = requests.post(url=url, data=payload, headers=headers)
+    response = requests.post(url=url, data=payload, headers=headers)  # ответ от сервера  сервера в формате
+
+
+    if list(response.text,) != ['{', '\n', ' ', ' ', ' ', '"', 'p', 'o', 's', 'i', 't', 'i', 'o', 'n', '"', ':', '\n',
+                               ' ', ' ', ' ', ' ', ' ', ' ', '{', '\n', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+                               '"', 'l', 'a', 't', 'i', 't', 'u', 'd', 'e', '"', ':', '5', '5', '.', '7', '5', '3', '2',
+                               '1', '5', '7', '8', '9', '7', '9', '4', '9', '2', ',', '\n', ' ', ' ', ' ', ' ', ' ',
+                               ' ', ' ', ' ', ' ', '"', 'l', 'o', 'n', 'g', 'i', 't', 'u', 'd', 'e', '"', ':', '3', '7',
+                               '.', '6', '2', '2', '5', '0', '5', '1', '8', '7', '9', '8', '8', '2', '8', ',', '\n',
+                               ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '"', 'a', 'l', 't', 'i', 't', 'u', 'd', 'e',
+                               '"', ':', '0', '.', '0', ',', '\n', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '"',
+                               'p', 'r', 'e', 'c', 'i', 's', 'i', 'o', 'n', '"', ':', '1', '0', '0', '0', '0', '0', '.',
+                               '0', ',', '\n', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '"', 'a', 'l', 't', 'i',
+                               't', 'u', 'd', 'e', '_', 'p', 'r', 'e', 'c', 'i', 's', 'i', 'o', 'n', '"', ':', '3', '0',
+                               '.', '0', ',', '\n', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '"', 't', 'y', 'p',
+                               'e', '"', ':', '"', 'i', 'p', '"', '\n', ' ', ' ', ' ', ' ', ' ', ' ', '}', '\n', '}']:
+        response_list.append(response.text)
+        print(cid)
+
+# добавляем ответ в список response_list
+
+    # if response.text.find('75321578979492') > 0:
+    #     response_list.append(response.text)
+    #     print(cid)
+    #
+    # else:
+    #     break
 
 
 
-    # преобразовани response в текст
-    response_txt = response.text
-
-    # добавляем ответ в
-    response_list.append(response_txt)
-    response_set = set(response_list)
-
-    file.write(response_txt)
 
 
-print(response_set)
+response_set = set(response_list)  # преобразование списка response list в множество для создания уникальных
+# значений ответов response
+
+file.write(str(response_set)) # запись множества в файл
+
 file.close()
 # response = requests.post('https://httpbin.org/post', data={'key':'value'})
 """" ответ на несуществующую базовую станцию """
-
-
-
--- key.py
+++ key.py
@@ -1,3 +1,1 @@
-def key_yandex():
-    global key
-    key = "AAWW1l0BAAAA-ySjGQIAJGp8BARUCie1PUbaznYu58C8xzcAAAAAAAAAAABF0LkL4ONOCjhC_WbHUrN__bCa6w=="
+key = "AAWW1l0BAAAA-ySjGQIAJGp8BARUCie1PUbaznYu58C8xzcAAAAAAAAAAABF0LkL4ONOCjhC_WbHUrN__bCa6w=="
